1b340e0037255c0802b9c5fc7d32074aaf5c37e6
wojtr <rwojtane@purdue.edu>
2018-02-11 16:28:32 -0500

    Created Lyrics class in models.py

--- app/models.py
+++ app/models.py
@@ -24,3 +24,31 @@ class User(db.Model):
 	@staticmethod
     	def get_all():
         	return User.query.all()	
+
+class Lyrics(db.Model):
+	__tablename__ = 'Lyrics'
+	
+	id = db.Column(db.Integer, primary_key=True)
+	songtitle = db.Column(db.String(255), unique=True, nullable=False)
+	songlyrics = db.Column(db.ARRAY(db.String(255)))
+	lyrictimestamps = db.Column(db.ARRAY(db.Integer))
+	
+	def __init__(self, songtitle, songlyrics, lyrictimestamps):
+		self.songtitle = songtitle
+		self.songlyrics = songlyrics
+		self.lyrictimestamps = lyrictimestamps
+		
+	def __repr__(self):
+		return 'id: {}, songtitle: {}'.format(self.id, self.songtitle)
+		
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
+		
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+		
+	@staticmethod
+	def get_all():
+		return Lyrics.query.all()
