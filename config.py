5aae71d3c5cccfae08b2f024f1ed4067f300e4a1
Noah Ignacio Smith <noahignaciosmith@gmail.com>
2018-02-11 18:50:44 -0500

    Added README which contains instructions on setting up the Dev environment. Modified __init__.py to require FLASK_CONFIG to be specified.

--- config.py
+++ config.py
@@ -1,25 +1,19 @@
 class Config(object):
-    	DEBUG = False
-    	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cxYv23gm9PjYfMLtWd@livelyrics.cnfthxsaxcoe.us-east-2.rds.amazonaws.com/livelyrics'
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cxYv23gm9PjYfMLtWd@livelyrics.cnfthxsaxcoe.us-east-2.rds.amazonaws.com/livelyrics'
 
 
-class LocalDevelopment(Config):
-    	DEBUG = True
-    	SQALCHEMY_ECHO = True
+class Development(Config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
 
 
 class Production(Config):
-    	DEBUG = False
-    	SQALCHEMY_ECHO = False
-
-
-class ServerDevelopment(Config):
-	DEBUG = True
-    	SQALCHEMY_ECHO = True
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
 
 
 app_config = {
-    	'local': LocalDevelopment,
-	'server': ServerDevelopment,
-	'production': Production
+    'development': Development,
+    'production': Production
 }
