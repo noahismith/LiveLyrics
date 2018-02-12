36deb61f3c6ceb8923d8baff471d9320484ce893
Noah Ignacio Smith <noahignaciosmith@gmail.com>
2018-02-11 19:44:17 -0500

    Added blueprint routes for each html page Edited href tags to correspond to blueprint routes

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
-import os
-from flask import Flask, Blueprint, render_template
-from flask_sqlalchemy import SQLAlchemy
-from app import db
+from flask import Blueprint, render_template
 
 views_blueprint = Blueprint('views', __name__)
 
 @views_blueprint.route("/")
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+@views_blueprint.route("/lyrics")
+def lyrics():
+    return render_template('lyrics.html')
+
+
+@views_blueprint.route("/users")
+def users():
+    return render_template("users.html")
+
+
+@views_blueprint.route("/songs")
+def songs():
+    return render_template("songs.html")
+
+
+@views_blueprint.route("/contact")
+def contact():
+    return render_template("contact.html")
