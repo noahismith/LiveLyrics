75712bd7092ef782436eba2ad9738abae3ce576a
wojtr <rwojtane@purdue.edu>
2018-03-05 17:46:04 -0500

    fixed errors withing rating module and has now been tested. Also added a few helper functions to help with debugging

--- app/lyricratingBp.py
+++ app/lyricratingBp.py
@@ -16,9 +16,8 @@ def rate():
     lyrics_id = payload['lyric_id']
     rater_id = payload['rater_id']
 
-    lyric_sheet = db.session.query(Lyrics).filter_by(id=lyrics_id).first()
-    rater = db.session.query(User).filter_by(id=rater_id).frist()
-
+    lyric_sheet = db.session.query(Lyrics).filter_by(spotify_track_id=lyrics_id).first()
+    rater = db.session.query(User).filter_by(spotify_id=rater_id).first()
 
     if lyric_sheet is None:
         return jsonify({'result': False, 'error': "Lyric sheet not found in the database."})
@@ -26,10 +25,10 @@ def rate():
     if rater is None:
         return jsonify({'result': False, 'error': "User not found in the database."})
 
-    lyric_rating = db.session.query(LyricRating).filter(and_(LyricRating.lyrics_id == lyrics_id, LyricRating.rater_id == rater_id)).first()
+    lyric_rating = db.session.query(LyricRating).filter(and_(LyricRating.lyrics_id == lyric_sheet.id, LyricRating.rater_id == rater.id)).first()
 
     if lyric_rating is None:
-        lyric_rating = LyricRating(rating, lyrics_id, rater_id)
+        lyric_rating = LyricRating(rating, lyric_sheet.id, rater.id)
         lyric_rating.save()
         return jsonify({'result': True, 'error': ""})
 
@@ -40,39 +39,53 @@ def rate():
 	
 @lyricrating_blueprint.route("/getRating", methods=['POST'])
 def getRating():
-    payload - json.loads(request.data.decode())
-    lyricsId = payload['lyrics_id']
-    raterId = payload['rater_id']
+    payload = json.loads(request.data.decode())
+    lyrics_id = payload['lyric_id']
+    rater_id = payload['rater_id']
 	
-    lyricSheet = db.session.query(Lyrics).filter_by(id=lyricsId).first()
-    rater = db.session.query(User).filter_by(id=raterId).frist()
+    lyric_sheet = db.session.query(Lyrics).filter_by(spotify_track_id=lyrics_id).first()
+    rater = db.session.query(User).filter_by(spotify_id=rater_id).first()
 
 
-    if lyricSheet is None:
+    if lyric_sheet is None:
         return jsonify({'result': False, 'error': "Lyric sheet not found in the database."})
 
     if rater is None:
 	    return jsonify({'result': False, 'error': "User not found in the database."})
 
-    lyricRating = db.session.query(LyricRating).filter(and_(LyricRating.lyrics_id == lyrics_id, LyricRating.rater_id == rater_id)).first()
+    lyric_rating = db.session.query(LyricRating).filter(and_(LyricRating.lyrics_id == lyric_sheet.id, LyricRating.rater_id == rater.id)).first()
 
-    if lyricsRating is None:
+    if lyric_rating is None:
         return jsonify({'result': False, 'error': "The user has not put a rating on this lyric page."})
 		
-    return jsonify({'result': True, 'error': "", "rating": lyricRating.toJSON()})
+    return jsonify({'result': True, 'error': "", "rating": lyric_rating.toJSON()})
 	
 @lyricrating_blueprint.route("/getRatings", methods=['POST'])
 def getRatings():
-	payload = json.loads(request.data.decode())
-	lyricsId = payload['lyrics_id']
+    payload = json.loads(request.data.decode())
+    lyrics_id = payload['lyric_id']
 	
-	lyricRatings = db.session.query(LyricRating).filter(LyricRating.lyricsId == lyrics_id)
+    lyric_sheet = db.session.query(Lyrics).filter_by(spotify_track_id=lyrics_id).first()
 	
-	if lyricRatings is None:
-		return jsonify({'result': False, 'error': "The requested lyrics page has no ratings."})
+    if lyric_sheet is None:
+        return jsonify({'result': False, 'error': "Lyric sheet not found in the database."})
+	
+    lyricRatings = db.session.query(LyricRating).filter(LyricRating.lyrics_id == lyric_sheet.id)
+	
+    if lyricRatings is None:
+	    return jsonify({'result': False, 'error': "The requested lyrics page has no ratings."})
 		
-	allRatingsList = []
-	for rating in LyricsRatings:
+    allRatingsList = []
+    for rating in lyricRatings:
 	    allRatingsList.append(rating.toJSON())
 		
-	return jsonify({'result': True, 'error': "", 'ratings': allRatingsList})
+    return jsonify({'result': True, 'error': "", 'ratings': allRatingsList})
+
+@lyricrating_blueprint.route("/getAllRatings", methods=['GET'])	
+def getAllRatings():
+    lyricRatings = LyricRating.get_all()
+    ratings_list = []
+    for rating in lyricRatings:
+        ratings_list.append(rating.toJSON())
+    return jsonify({'result': True, 'error': "", 'ratings': ratings_list})
+        
