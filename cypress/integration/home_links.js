52dd3df85c65cd5730a2e1a1bc67548565eb610c
womalley <womalley1495@gmail.com>
2018-03-12 23:48:39 -0400

    Final Frontend Testing

--- cypress/integration/home_links.js
+++ cypress/integration/home_links.js
@@ -28,14 +28,14 @@ describe('While on the home page, checks to make sure all navigation buttons go 
             .should('include', '/songs')
     })
 
-    it('Click Login button (checks login successful)', function () {
-        
-        cy.visit('127.0.0.1:5000')
-
-        cy.get('a').contains('Login').click()
 
 
+    it('Login Test: Input login info to Spotify API call (Fails means already logged in, try using your own Spotify credentials)', function () {
+        cy.visit('https://accounts.spotify.com/en/authorize?scope=streaming%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20user-read-email%20user-read-birthdate&redirect_uri=http:%2F%2F127.0.0.1:5000%2Flogin&response_type=code&client_id=91893049176646de8ec8994ea5cd0b27&show_dialog=false')
+        cy.get('a').contains('Log in to Spotify').click()
+        cy.get('input').first().type('LiveLyricsHelp@gmail.com')
+        cy.get('input#login-password.form-control.input-with-feedback.ng-pristine.ng-untouched.ng-empty.ng-invalid.ng-invalid-required').get('#login-password').type('cs408team11')
+        cy.get('button').click()
     })
-
 
 })
--- cypress/integration/users_links.js
+++ cypress/integration/users_links.js
@@ -1,34 +1,34 @@
 describe('While on the Users page, checks to make sure all navigation buttons go to the right link', function () {
 
     it('Click Home menu button', function () {
-        cy.visit('http://localhost:5000/users')
+        cy.visit('http://127.0.0.1:5000/users')
 
         cy.get('ul').contains('Home').click()
 
         cy.url()
-            .should('eq', 'http://localhost:5000/')
+            .should('eq', 'http://127.0.0.1:5000/')
     })
 
     it('Click Songs menu button', function () {
-        cy.visit('http://localhost:5000/users')
+        cy.visit('http://127.0.0.1:5000/users')
 
         cy.get('ul').contains('Songs').click()
 
         cy.url()
-            .should('eq', 'http://localhost:5000/songs')
+            .should('eq', 'http://127.0.0.1:5000/songs')
     })
 
     it('Click Lyrics menu button', function () {
-        cy.visit('http://localhost:5000/users')
+        cy.visit('http://127.0.0.1:5000/users')
 
         cy.get('ul').contains('Add Lyrics').click()
 
         cy.url()
-            .should('eq', 'http://localhost:5000/lyrics')
+            .should('eq', 'http://127.0.0.1:5000/lyrics')
     })
 
     it('Click Contact Us menu button', function () {
-        cy.visit('http://localhost:5000/users')
+        cy.visit('http://127.0.0.1:5000/users')
 
         cy.get('ul').contains('Contact Us').click()
     })
